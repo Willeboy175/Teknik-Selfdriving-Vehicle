433ea3c5c004f9325716021b27671d48d502486b
AntonBaksa <anton.baksa@elev.ga.lbs.se>
2023-12-07 15:14:24 +0100

    Update Motorer.ino

--- Motorer/Motorer.ino
+++ Motorer/Motorer.ino
@@ -1,9 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+// Motor A connections
+int enA = 9;
+int in1 = 14;
+int in2 = 27;
+// Motor B connections
+int enB = 3;
+int in3 = 16;
+int in4 = 17;
 
+void setup() {
+	// Set all the motor control pins to outputs
+	//pinMode(enA, OUTPUT);
+	//pinMode(enB, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	
+	// Turn off motors - Initial state
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	directionControl();
+	delay(1000);
 
 }
+
+// This function lets you control spinning direction of motors
+void directionControl() {
+	// Set motors to maximum speed
+	// For PWM maximum possible values are 0 to 255
+	
+
+	// Turn on motor A & B
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, HIGH);
+	digitalWrite(in4, LOW);
+	delay(2000);
+	
+	// Now change motor directions
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+	delay(2000);
+	
+	// Turn off motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
+
+
+
+
+
